cc63d9283fae61d3e6260875cdf07d590e8eef2e
Renato Cunha <cunhamrenato@gmail.com>
2025-09-16 03:54:31 -0300

    Switching to Android only

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    cfg_aliases::cfg_aliases! {
-        win: { target_os = "windows" },
-        andr: { target_os = "android" }
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-#[cfg(win)]
-mod win;
-
-mod common;
-
 #[tokio::main]
 async fn main() {
-    #[cfg(win)]
-    win::test().await.unwrap();
+    
 }
